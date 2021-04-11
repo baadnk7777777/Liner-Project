2d30383cf5a9119670da8f8e049ff6c296b1dbe0
Meliodas <banktv76@gmail.com>
2021-04-12 01:19:33 +0700

    Full-Stack

--- RREF.cpp
+++ RREF.cpp
@@ -39,15 +39,22 @@ int main ()
 	int row = M;  //3
 	int	cols = N; //4
 	int lead = 0;
+	int rowop =0;
+	
 	
 	while(lead < row) 
 	{
+		
 		float divisor, multiplier;
 		
 		for(int r=0;r<row;r++) //Run each row . 
-		{
+		{	
+			rowop += 1;
 			divisor = matrix[lead][lead];
 			multiplier = matrix[r][lead] / matrix[lead][lead];
+			
+			cout << "=============== " << "Rowoparation: " << rowop << " ===============" <<endl;
+			cout << endl;
 			
 			for(int c=0;c<cols;c++) // Run each cols
 			{
@@ -81,22 +88,39 @@ int main ()
 							}
 						cout << endl;
 				}
-				cout << "=============== " << "Row: " << r+1 << " ===============" <<endl;
-				cout << endl;					
+									
 		}
-		lead ++;
-	}
-	
-		for(i=0;i<M;i++)
-	{
-		cout << " | ";
-		for(j=0;j<N;j++)
+		
+		//check 
+		
+		for(int k=0;k<row;k++)
 		{
-			cout << matrix[i][j] << "      ";
+			int check =0;
+			for(int b=0;b<cols;b++)
+			{
+				if(matrix[k][b] == 0)
+			{
+				check++;
+//				cout << "check" << check << endl;
+//				cout << matrix[k][b] << endl;
+			}
+						if(check == cols)
+		{
+			cout << "Stop!! Because Have All Zero Inlast line !" << endl;
+			return 0;
 		}
-		cout << endl;
+			}
+
+		}
+		
+		
+		lead ++;
+		
 	}
-	
+	if(lead == row)
+	{
+		cout << "Success Slove! ! !" << endl;
+	}
 	return 0;
 	
 }
